cb557f8c88d84202261e96e5b043bcf93199bca6
jgomez <jgomez@eusoc.upm.es>
2018-12-19 11:41:42 +0100

    First version of functional MCU RTC drivers

---
+++ cores/msp430/RTC.cpp
@@ -0,0 +1,144 @@
+/*
+  ************************************************************************
+  *	RTC.cpp
+  * 
+  *
+  *
+  ***********************************************************************
+  
+  Created in 2019 by Theia Space.
+*/
+
+//Only RTC C by now
+
+#include "RTC.h"
+#include <Arduino.h>
+
+
+RealTimeClock::RealTimeClock()
+{
+}
+
+void RealTimeClock::begin()
+{
+	UCSCTL6_L &= (~XCAP_3); //Sets minimum capacitance for XT1 RTC crystal oscillator
+	RTCCTL01 = RTCMODE; //Also clears RTCHOLD to enable RTC 	
+}
+
+void RealTimeClock::write(RtcTimestamp timeToSet)
+{
+	RTCYEAR = timeToSet.year & 0x0FFF;
+	RTCMON = timeToSet.month;
+	RTCDAY = timeToSet.dayOfMonth;
+	RTCDOW = timeToSet.dayOfWeek;
+	RTCHOUR = timeToSet.hours;
+	RTCMIN = timeToSet.minutes;
+	RTCSEC = timeToSet.seconds;
+	return;
+}
+
+RtcTimestamp RealTimeClock::read(void)
+{
+	RtcTimestamp readTime;
+	while (!(RTCCTL01 & RTCRDY));	//While RTCRDY is off, wait
+	readTime.seconds = RTCSEC;
+	readTime.minutes = RTCMIN;
+	readTime.hours = RTCHOUR;
+	readTime.dayOfWeek = RTCDOW;
+	readTime.dayOfMonth = RTCDAY;
+	readTime.month = RTCMON;
+	readTime.year = (RTCYEAR & 0x0FFF);
+	return (readTime);
+}
+
+RtcTimestamp RealTimeClock::readUnreliable(void)
+{
+	RtcTimestamp readTime;
+	readTime.seconds = RTCSEC;
+	readTime.minutes = RTCMIN;
+	readTime.hours = RTCHOUR;
+	readTime.dayOfWeek = RTCDOW;
+	readTime.dayOfMonth = RTCDAY;
+	readTime.month = RTCMON;
+	readTime.year = (RTCYEAR & 0x0FFF);
+	return (readTime);
+}
+
+uint8_t RealTimeClock::available()
+{
+	if (RTCCTL01 & RTCRDY) 
+	{
+		return RTC_SAFE_TO_READ;
+	}
+	return RTC_UNSAFE_TO_READ;
+}
+
+uint8_t RealTimeClock::status()
+{
+	if (RTCCTL01 & RTCHOLD)
+	{
+		return 0;
+	}
+	return 1;
+}
+
+void RealTimeClock::disable()
+{
+	RTCCTL01 = RTCHOLD; //Sets whole RTCCTL01 to reset state
+	return;
+}
+
+void RealTimeClock::setMode(uint8_t mode)
+{
+	switch (mode) //Only changes if proper value is set
+	{
+		case RTC_BCD:
+		{
+		RTCCTL01|=RTCBCD; 
+		return;
+		}
+		case RTC_DECIMAL:
+		{
+		RTCCTL01&=(~RTCBCD);
+		return;
+		}
+		default:
+		return;
+	}
+	return;
+}
+
+void RealTimeClock::setCalibration(int8_t calibrationValue)
+{
+	//this way allows you to set the desired calibration value directly
+	if (calibrationValue<=0) //if value is negative, a decrement is desired (if it is zero, calibration is disabled)
+	{
+		calibrationValue*=-1; //change to positive, disable positive sign bit, mask and divide by two because each step is -2 ppm
+		RTCCTL23_L = (RTCCAL5_L | RTCCAL4_L |RTCCAL3_L | RTCCAL2_L | RTCCAL1_L | RTCCAL0_L) & (((uint8_t)calibrationValue) / 2);
+	}
+	else
+	{
+		//enable positive bit, mask and divide by 4 because each step is +4 ppm
+		RTCCTL23_L = RTCCALS_L | ((RTCCAL5_L | RTCCAL4_L | RTCCAL3_L | RTCCAL2_L | RTCCAL1_L | RTCCAL0_L) & (((uint8_t)calibrationValue) / 4));
+	}
+	return;
+}
+
+void RealTimeClock::setCalibrationOutput(uint8_t frequency)
+{
+	if ((frequency == RTC_CALIBRATION_SIGNAL_512HZ) || (frequency == RTC_CALIBRATION_SIGNAL_256HZ) || (frequency == RTC_CALIBRATION_SIGNAL_1HZ ))
+	{
+		//pin peripheral selection
+		//shared with ESP0 GPIO0 on OBC
+		P2DIR |= (1<<6);
+		P2SEL |= (1<<6);
+		RTCCTL23_H = frequency;
+		return;
+	}
+	//pin peripheral disable
+	//shared with ESP0 GPIO0 on OBC
+	P2SEL &= ~(1<<6);
+	P2DIR &= ~(1<<6);	
+	RTCCTL23_H = 0;
+	return;
+}
---
+++ cores/msp430/RTC.h
@@ -0,0 +1,92 @@
+/*
+  ************************************************************************
+  *	RTC.h
+  * 
+  *
+  *
+  ***********************************************************************
+  
+  *Created in 2019 by Theia Space.
+*/
+
+#ifndef RTC_h
+#define RTC_h
+
+#include <Energia.h> 
+#include <inttypes.h>
+#include <msp430.h>
+
+#define DEFAULT_YEAR 	2019
+#define DEFAULT_MONTH 	1
+#define DEFAULT_DAY 	1
+#define DEFAULT_DOW 	0
+#define DEFAULT_HOURS 	0
+#define DEFAULT_MINUTES 0
+#define DEFAULT_SECONDS 0
+
+#define RTC_DECIMAL 0
+#define RTC_BCD 	1
+
+#define RTC_UNSAFE_TO_READ 0
+#define RTC_SAFE_TO_READ 1
+
+#define RTC_CALIBRATION_SIGNAL_DISABLED	0
+#define RTC_CALIBRATION_SIGNAL_512HZ 	1
+#define RTC_CALIBRATION_SIGNAL_256HZ 	2
+#define RTC_CALIBRATION_SIGNAL_1HZ		3
+
+typedef struct
+{
+	uint16_t year;
+	uint8_t month;
+	uint8_t dayOfMonth;
+	uint8_t dayOfWeek;
+	uint8_t hours;
+	uint8_t minutes;
+	uint8_t seconds;
+} RtcTimestamp;
+
+
+
+class RealTimeClock
+{
+
+	public:
+	
+	// Instantiates a new RTC object 
+    RealTimeClock();
+	
+	//Enables RTC
+	void begin();
+	
+	//Writes timestamp into RTC registers
+	void write(RtcTimestamp timeToSet);	
+	
+	//Retrieves timestamp from RTC registers, checking if no registers are changing
+	RtcTimestamp read();
+	
+	//Retrieves tiemstamp without checking if the registers are changing
+	RtcTimestamp readUnreliable();
+	
+	//Checks if RTC can be read safely
+	uint8_t available();
+	
+	//Checks if RTC is enabled
+	uint8_t status();
+	
+	//Turns RTC off
+	void disable();
+	
+	//Sets RTC into BCD or decimal mode
+	void setMode(uint8_t mode);
+	
+	//Allows user to trim RTC frequency
+	void setCalibration(int8_t calibrationValue);
+	
+	//Enables or disables calibration output waverform
+	void setCalibrationOutput(uint8_t frequency);
+	
+				   
+};
+
+#endif
